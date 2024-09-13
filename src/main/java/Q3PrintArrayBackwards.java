81229d5047f68f4f14de90e13dbb02934fe3c0f6
DavidShodipe <DS986484@wcupa.edu>
2024-09-12 23:20:43 -0400

    kk

--- src/main/java/Q3PrintArrayBackwards.java
+++ src/main/java/Q3PrintArrayBackwards.java
@@ -2,13 +2,13 @@ public class Q3PrintArrayBackwards {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length - 1);
+        printRecursion(array.length);
     }
-    private static void printRecursion(int index) {
-        if (index < 0) {
-            return;
-        }
-        printRecursion(index - 1);
-        System.out.println(array[index]);
+
+    private static void printRecursion(int i) {
+        // TODO: Implement the recursive method to print array elements backwards
+        // Hint: The base case is when i <= 0
+        // Hint: Print the element after the recursive call
     }
 }
+// src/main/java/Q4ReverseString.java
