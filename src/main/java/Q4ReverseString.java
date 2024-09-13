77b23d9a0931d473cccf93d3189d9a49eeaeceee
DavidShodipe <DS986484@wcupa.edu>
2024-09-13 00:01:13 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -2,9 +2,9 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         if (s == null || s.length() <= 1) {
-            return s; // Base case: if the string is null or has 0 or 1 character, return it as is
+            return s; 
         }
-        return reverse(s.substring(1)) + s.charAt(0); // Recursive case: reverse the substring and append the first character
+        return reverse(s.substring(1)) + s.charAt(0); 
     }
 
     public static void main(String[] args) {
